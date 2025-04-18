31803a5d470f8471653e5d5f319c4e8668937557
Sara Winsemius <swinsemius@ucdavis.edu>
2025-04-18 14:53:59 -0700

    Fix figure for review

--- scripts/sandbox/figure_BAloss_dSWIR_precipnorth.R
+++ scripts/sandbox/figure_BAloss_dSWIR_precipnorth.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 ard <- read.csv("VP/severity_tmp/data/saved/ARD_nocoords.csv")
 
 summary(lm(formula = pcnt_ba_mo ~ zScorePrecip1, data = ard[, c("pcnt_ba_mo", "zScorePrecip1")]))
@@ -32,6 +34,30 @@ ggplot(ard[ard$zScorePrecip1<0,], aes(x = dswir2swir1, y = pcnt_ba_mo)) +
   labs(y="Basal area loss (%)", x="dSWIR2:SWIR1") +
   theme_minimal() +
   theme(legend.position="bottom", legend.direction="vertical")
+ggplot(ard, aes(x = dswir2swir1, y = pcnt_ba_mo, col = zScorePrecip1 > 0)) +
+  geom_point(aes(alpha=0.25), show_guide = FALSE) +
+  
+  # Smoothing split by zScorePrecip1 > 0
+  geom_smooth() +
+  
+  # Smoothing over the entire dataset (ignores color grouping)
+  geom_smooth(
+    inherit.aes = FALSE,
+    data = ard,
+    aes(x = dswir2swir1, y = pcnt_ba_mo),
+    method = "gam",
+    color = "black"
+  ) +
+  
+  scale_color_manual(
+    name   = "Z-score precipitation",
+    values = c("TRUE" = "#0C7BDC", "FALSE" = "#FFC20A"),
+    labels = c("Negative", "Positive")
+  ) +
+  labs(y = "Basal area loss (%)", x = "dSWIR2:SWIR1") +
+  theme_minimal() +
+  theme(legend.position="bottom", legend.direction="vertical")
+
 ggplot(ard, aes(x = dswir2swir1, y = pcnt_ba_mo, col = zScorePrecip1 > 0)) +
   geom_point(aes(alpha=0.25), show_guide = FALSE) +
   geom_smooth() +
@@ -70,7 +96,7 @@ p1 <- ggplot(ard, aes(x = dswir2swir1, y = pcnt_ba_mo, col = zScorePrecip1 > 0))
     values = c("TRUE" = "#0C7BDC", "FALSE" = "#FFC20A"),
     labels = c("Drier than average","Wetter than average")
   ) +
-  labs(y = "Basal area loss (%)", x = "dSWIR2/SWIR1") +
+  labs(y = "Basal area loss", x = "dSWIR2/SWIR1") +
   theme_bw() +
   theme(legend.position = "bottom", legend.title=element_blank(), legend.direction = "vertical") #, legend.direction = "vertical"
 p1
@@ -83,7 +109,7 @@ p2 <- ggplot(ard, aes(x = dswir2swir1, y = pcnt_ba_mo, col = northness > 0)) +
     values = c("TRUE" = "#0C7BDC", "FALSE" = "#FFC20A"),
     labels = c("South-facing", "North-facing")
   ) +
-  labs(y = "Basal area loss (%)", x = "dSWIR2/SWIR1") +
+  labs(y = "Basal area loss", x = "dSWIR2/SWIR1") +
   theme_bw() +
   theme(legend.position = "bottom", legend.title=element_blank(), legend.direction = "vertical")#, legend.direction = "vertical"
 
@@ -108,7 +134,7 @@ combined_plot <- combined_plot +
   )
 combined_plot
 # Save Combined Plot
-ggsave("VP/severity_tmp/plots/BA_dSWIR_combined2.png", 
+ggsave("VP/severity_tmp/plots/BA_dSWIR_combined3.png", 
        combined_plot, width = 6, height = 4, units = "in", dpi = 300)
 
 
