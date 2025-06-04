5f0ed4dd96c2a1897f7179e14d0a97e3ad3dbeca
Sara Winsemius <swinsemius@ucdavis.edu>
2025-06-04 16:00:30 -0700

    ard, clean analysis and some tables, viz
    Cleaned up from 06, 08.2, 09, 11 from sandbox

--- scripts/deploy/01_make_ard.R
+++ scripts/deploy/01_make_ard.R
@@ -8,13 +8,15 @@ library(glue)
 data_dir <- "research/severity/data/"
 data_dir1 <- "VP/severity_tmp/data/saved/"
 
-
+### to do:
+## remove NPS plots from GEE code
+## include fuzzed NPS locations below when making spatial ARD
 
 ##########################################################
 ################# Spectral data ##################
 ##########################################################
 
-spectral <- read.csv(paste0(data_dir, "GEE/spectral_08242024.csv"))
+spectral <- read.csv(paste0(data_dir, "input/spectral_05282025.csv"))
 
 spectral$system.index <- NULL
 spectral$.geo <- NULL
@@ -24,7 +26,7 @@ spectral$.geo <- NULL
 ################# Terraclimate ##################
 ##########################################################
 
-climate <- read.csv(paste0(data_dir, "climate_05282025.csv"))
+climate <- read.csv(paste0(data_dir, "input/climate_05282025.csv"))
 
 climate$system.index <- NULL
 climate$.geo <- NULL
@@ -34,7 +36,7 @@ climate$.geo <- NULL
 ################# Topography ##################
 ##########################################################
 
-topo <- read.csv(data_dir, "GEE/topo_05282025.csv")
+topo <- read.csv(paste0(data_dir, "input/topo_05282025.csv"))
 
 topo$system.index <- NULL
 topo$.geo <- NULL
@@ -44,57 +46,37 @@ topo$.geo <- NULL
 ################# Merge datasets ##################
 ##########################################################
 
-
 allgee <- merge(spectral, climate, by=c("UniqueID", "PlotID", "Dataset", "FireYear", "Start_Day", "End_Day", "Unit", "ID", "YrFireName", "pcnt_ba_mo"))
 
 allgee <- merge(allgee, topo, by=c("UniqueID", "PlotID", "Dataset", "FireYear", "Start_Day", "End_Day", "Unit", "ID", "YrFireName", "pcnt_ba_mo"))
+
 
 allgee$aspectRad <- NULL
 allgee$lat <- NULL
 
 names(allgee)
+
 # are there duplicates??
-duplicates_count <- alldata %>%
+duplicates_count <- allgee %>%
   group_by(Dataset, Unit, YrFireName, ID, PlotID) %>%
   summarise(n = n(), .groups = 'drop') %>%
   filter(n > 1)
 duplicates_count
 
-## To get the total number of duplicated rows
-#total_duplicated_rows <- sum(duplicates_count$n) - nrow(duplicates_count)
-#total_duplicated_rows
-
-#alldata <- alldata[alldata$PlotID != "Wallow9_1_118",]
-
 ##########################################################
 ################# Save data ##################
 ##########################################################
 
-##### Dataframe
-# 
-# alldata$UniqueID <- paste0(alldata$Dataset, "_", alldata$PlotID)
-# 
-# alldata[alldata$Dataset=="Hood",]$UniqueID <- 
-#   paste0(alldata[alldata$Dataset=="Hood",]$UniqueID, "_", substring(alldata[alldata$Dataset=="Hood",]$YrFireName, 8))
-# alldata[alldata$Dataset=="Davis",]$UniqueID <- 
-#   paste0(alldata[alldata$Dataset=="Davis",]$UniqueID, "_", substring(alldata[alldata$Dataset=="Davis",]$YrFireName, 8))
-
-length(unique(allgee$UniqueID))
-nrow(allgee)
-
-# if there's a mismatch, look at the data to see how to make the UniqueID column truly unique
-#duplicated_rows <- alldata[duplicated(alldata$UniqueID) | duplicated(alldata$UniqueID, fromLast = TRUE), ]
-
 ard <- allgee[,!names(allgee) %in% c("FireYear", "Start_Day", "End_Day", "Unit", "ID")]
 
 ### Write ARD dataframe without coords
-write.csv(ard, paste0(data_dir, "ARD_nocoords.csv"), row.names = FALSE)
+write.csv(ard, paste0(data_dir, "saved/ARD_nocoords_06042025.csv"), row.names = FALSE)
 
 
-##### ARD with plot locations #####
+##### ARD with plot locations and ecoregion #####
 
 # point location data
-coords <- vect(paste0(data_dir, "allcoords_withbaloss_v7.shp"))
+coords <- vect(paste0(data_dir, "input/allcoords_withbaloss_v7.shp")) 
 
 # combine 
 ardcoords <- merge(coords, ard, by=c("UniqueID", "PlotID", "YrFireName", "Dataset", "pcnt_ba_mo"))
@@ -104,7 +86,15 @@ names(ardcoords)
 
 ardcoords <- ardcoords[,!names(ardcoords) %in% c("FireYear", "Start_Day", "End_Day", "Unit", "ID")]
 
+## extract ecoregion
+ecoregions <- vect(paste0(data_dir, "input/Ecoregions2017/Ecoregions2017.shp"))
+ecoregions <- ecoregions[ecoregions$REALM=="Nearctic",] # subset 
+
+biome_info <- terra::extract(ecoregions[, "ECO_NAME"], ardcoords)
+
+ardcoords$ecoregion <- biome_info$ECO_NAME
+
 # save ARD
-writeVector(ardcoords, paste0(data_dir, "ARD_05282025.gpkg"))
+writeVector(ardcoords, paste0(data_dir, "saved/ARD_06042025.gpkg"))
 
 
---
+++ scripts/deploy/05_ecoregion_models.R
@@ -0,0 +1,56 @@
+
+## Get the best model for each ecoregion
+# Also some code for Pareto frontier, which we don't use in the paper
+
+library(ggplot2)
+
+cpi_results_er = data.table::fread(paste0(data_dir, "saved/conditional-predictive-impact-results_v5.1.csv"))
+names(cpi_results_er)
+
+lowest_rmse <- cpi_results_er %>%
+  group_by(ecoregion, mtry, sample.fraction, min.node.size, important_variable_rf_formula, rmse_important_variables_overall, r2_important_variables_overall) %>%
+  summarize()
+
+head(lowest_rmse)
+
+head(cpi_results_er)
+cpi_results = cpi_results_er |>
+  group_by(ecoregion) %>%
+  dplyr::select(-Variable, -CPI, -SE, -test, -statistic, -estimate, -p.value, -ci.lo) |>
+  unique()
+
+# Get the model with the best R2 (overall = plot-based calc rather than avg of fold R2s)
+bestr2 <- cpi_results %>%
+  group_by(ecoregion) %>%
+  slice_max(r2_important_variables_overall, n = 1, with_ties = FALSE) %>%
+  ungroup()
+names(bestr2)
+bestr2table <- bestr2[, c(1:5, 17,24,25)]
+
+bestr2table$r2_important_variables_overall <- signif(bestr2table$r2_important_variables_overall, digits=3)
+bestr2table$rmse_important_variables_overall <- signif(bestr2table$rmse_important_variables_overall, digits=3)
+head(bestr2table)
+
+write.csv(bestr2table, paste0(data_dir, "saved/ecoregion_models_bestr2.csv"))
+
+
+##########################################
+##### Pareto frontier plot #####
+##########################################
+
+# Plot Pareto frontier of cross-fold mean R2 and overall R2 of important variable reduced model
+# We didn't use the Pareto frontier for selecting the best model, but one could!
+
+r2_pareto_front = rPref::psel(
+  df = cpi_results,
+  pref = rPref::high(r2_important_variables_overall) * rPref::high(r2_important_variables)
+)
+
+ggplot2::ggplot(cpi_results, ggplot2::aes(x = r2_important_variables_overall, y = r2_important_variables, color=ecoregion)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_point(data = r2_pareto_front, shape=2, size = 3) +
+  ggplot2::theme_bw() +
+  labs(y="R2 - mean of spatial folds", x="R2 - overall mean across plots")
+ggsave(paste0(fig_dir, "pareto_frontier_ecoregional.png"), width = 6, height = 3, units = "in")
+
+
