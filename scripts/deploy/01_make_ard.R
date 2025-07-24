8b31027eb452f780c6998a1d95fb9f718a6ba7e9
Sara Winsemius <swinsemius@ucdavis.edu>
2025-07-24 09:46:47 -0700

    some more script cleaning for ard and ravg
    see list of to-dos at top of the 01 script

--- scripts/deploy/01_make_ard.R
+++ scripts/deploy/01_make_ard.R
@@ -6,40 +6,31 @@ library(terra)
 library(glue)
 
 data_dir <- "research/severity/data/"
-data_dir1 <- "VP/severity_tmp/data/saved/"
 
-fileap <- "ravg_test"
+# use the same name at the end of spectral_, climate_, and topo_ in GEE
+filename <- "05282025"
 
 ### to do:
 ## remove NPS plots from GEE code
 ## include fuzzed NPS locations below when making spatial ARD
+## remove unit, id, dataset, and yrfirename (others? plotid?) from that file
+## make sure the startday format is the same on gee and here. I think startDay and endDay is a nice format
 
-##########################################################
 ################# Spectral data ##################
-##########################################################
 
-spectral <- read.csv(paste0(data_dir, "input/spectral_", fileap, ".csv"))
-
+spectral <- read.csv(paste0(data_dir, "input/spectral_", filename, ".csv"))
 spectral$system.index <- NULL
 spectral$.geo <- NULL
 
-
-##########################################################
 ################# Terraclimate ##################
-##########################################################
 
 climate <- read.csv(paste0(data_dir, "input/climate_", fileap, ".csv"))
-
 climate$system.index <- NULL
 climate$.geo <- NULL
 
-
-##########################################################
 ################# Topography ##################
-##########################################################
 
 topo <- read.csv(paste0(data_dir, "input/topo_", fileap, ".csv"))
-
 topo$system.index <- NULL
 topo$.geo <- NULL
 
@@ -51,21 +42,13 @@ topo$.geo <- NULL
 # by=c("UniqueID", "PlotID", "Dataset", "FireYear", "Start_Day", "End_Day", "Unit", "ID", "YrFireName", "pcnt_ba_mo")
 
 allgee <- merge(spectral, climate, by=c("UniqueID", "FireYear", "pcnt_ba_mo", "Fire"))
-
 allgee <- merge(allgee, topo, by=c("UniqueID", "FireYear", "pcnt_ba_mo", "Fire"))
-
 
 allgee$aspectRad <- NULL
 allgee$lat <- NULL
 
 names(allgee)
 
-# are there duplicates??
-duplicates_count <- allgee %>%
-  group_by(Dataset, Unit, YrFireName, ID, PlotID) %>%
-  summarise(n = n(), .groups = 'drop') %>%
-  filter(n > 1)
-duplicates_count
 
 ##########################################################
 ################# Save data ##################
@@ -75,7 +58,7 @@ ard <- allgee[,!names(allgee) %in% c("FireYear", "Start_Day", "End_Day", "Unit",
 
 ### Write ARD dataframe without coords
 # write.csv(ard, paste0(data_dir, "saved/ARD_nocoords_06042025.csv"), row.names = FALSE)
-write.csv(ard, paste0(data_dir, "saved/ARD_", fileap, ".csv"), row.names = FALSE)
+write.csv(ard, paste0(data_dir, "saved/ARD_", filename, ".csv"), row.names = FALSE)
 
 
 ##### ARD with plot locations and ecoregion #####
@@ -93,17 +76,16 @@ ardcoords
 names(ardcoords)
 
 ardcoords <- ardcoords[,!names(ardcoords) %in% c("FireYear", "Start_Day", "End_Day", "Unit", "ID", "Fire")]
-terra::writeVector(ardcoords, paste0(data_dir, "saved/ARD_", fileap, ".gpkg"))
 
 ## extract ecoregion
 ecoregions <- vect(paste0(data_dir, "input/Ecoregions2017/Ecoregions2017.shp"))
-ecoregions <- ecoregions[ecoregions$REALM=="Nearctic",] # subset 
+ecoregions <- ecoregions[ecoregions$REALM=="Nearctic",] # subset to run faster
 
 biome_info <- terra::extract(ecoregions[, "ECO_NAME"], ardcoords)
 
 ardcoords$ecoregion <- biome_info$ECO_NAME
 
 # save ARD
-writeVector(ardcoords, paste0(data_dir, "saved/ARD_06042025.gpkg"))
+writeVector(ardcoords, paste0(data_dir, "saved/ARD_", filename, ".gpkg"))
 
 
